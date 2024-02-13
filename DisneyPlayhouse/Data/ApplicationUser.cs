e8ebeaaea4c5b97a86073b803293a085fcdc8075
Chin <Damienchin1218@gmail.com>
2024-02-13 15:03:08 +0700

    Added Registration for Member
    - Database tables created
    - Removed Email confirmation
    - Added CurrencyType and Roles

--- DisneyPlayhouse/Data/ApplicationUser.cs
+++ DisneyPlayhouse/Data/ApplicationUser.cs
@@ -5,6 +5,6 @@ namespace DisneyPlayhouse.Data
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        public string CurrencyType { get; set; }
     }
-
 }
---
+++ DisneyPlayhouse/Data/Migrations/20240213074801_CurrencyType.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DisneyPlayhouse.Migrations
+{
+    /// <inheritdoc />
+    public partial class CurrencyType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CurrencyType",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CurrencyType",
+                table: "AspNetUsers");
+        }
+    }
+}
