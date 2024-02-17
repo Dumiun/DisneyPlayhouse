6c4a9a1ff6145226d9a89dd492f85541dfbcc7e6
Chin <Damienchin1218@gmail.com>
2024-02-17 19:20:10 +0700

    Added Page Report (Not functional yet)
    - Added the template for Page Report
    -Updated some of the Resources files

---
+++ DisneyPlayhouse/Models/ReportHistoryModel.cs
@@ -0,0 +1,9 @@
+﻿namespace DisneyPlayhouse.Models
+{
+    public class ReportHistoryModel
+    {
+        public string ReportForId { get; set; }
+        public DateTime FromDate { get; set; }
+        public DateTime ToDate { get; set; }
+    }
+}
