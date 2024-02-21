48bdf5fd391dc046b3bae26af43bee885bbc071c
Chin <Damienchin1218@gmail.com>
2024-02-21 18:34:39 +0700

    Updated Page Report

---
+++ DisneyPlayhouse/Models/PageReportChildTotalViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace DisneyPlayhouse.Models
+{
+    public class PageReportChildTotalViewModel
+    {
+        public string MemberId { get; set; }
+        public string Role { get; set; } = "Member";
+        public double TotalBig { get; set; }
+        public double TotalSmall { get; set; }
+        public double Commsion { get; set; }
+        public double TotalAmount { get; set; }
+        public double StrikeAmount { get; set; }
+        public double Balance { get; set; }
+    }
+}
---
+++ DisneyPlayhouse/Models/PageReportTotalViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace DisneyPlayhouse.Models
+{
+    public class PageReportTotalViewModel
+    {
+        public DateTime DrawDate { get; set; }
+        public double TotalBig { get; set; }
+        public int TotalSmall { get; set; }
+        public double TotalAmount { get; set; }
+        public double TotalStrike { get; set; }
+        public double Balance { get; set; }
+    }
+}
