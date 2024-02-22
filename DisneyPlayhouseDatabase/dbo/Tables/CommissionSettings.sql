826bc57bdf3049e84ad009449c38b926acf1c08e
Chin <Damienchin1218@gmail.com>
2024-02-22 16:25:20 +0700

    Minor Updates and draft of Manage Member Page

---
+++ DisneyPlayhouseDatabase/dbo/Tables/CommissionSettings.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[CommissionSettings]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [MemberId] NVARCHAR(20) NOT NULL, 
+    [CommsPercentage] DECIMAL(4, 2) NULL, 
+    [LastUpdatedOn] DATETIME2 NOT NULL
+)
