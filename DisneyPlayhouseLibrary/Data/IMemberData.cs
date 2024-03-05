049f26242aefce26f9a2ae590efd6485b0f77a0a
Chin <Damienchin1218@gmail.com>
2024-03-05 16:26:45 +0700

    Base Setup for v0.0.3 and add Roles to EF

--- DisneyPlayhouseLibrary/Data/IMemberData.cs
+++ DisneyPlayhouseLibrary/Data/IMemberData.cs
@@ -5,13 +5,21 @@ namespace DisneyPlayhouseLibrary.Data
     public interface IMemberData
     {
         Task CreateNewMemberDetails(ILib_MemberDetailsModel info);
+
         Task CreateNewMemberRelationship(ILib_MemberDetailsModel info);
+
         Task<double> GetCreditOfUser(string userId);
+
         Task<List<ILib_ListOfChildIdModel>> GetListOfChildId(string currentUserId);
+
         Task<List<ILib_ListOfChildIdModel>> GetListOfDirectChildId(string currentUserId);
+
         Task<List<ILib_ListOfParentIdModel>> GetListOfParentId();
+
         Task<double> GetTicketCommsOfUser(string userId);
+
         Task UpdatedMemberCredit(string user, double newValue);
+
         Task UpdatedMemberTicketComms(string user, double newValue);
     }
 }
